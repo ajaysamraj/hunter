ee1a3a132c0d78521183ede1592e4d4ce6a58e96
ajaysamraj <34670366+ajaysamraj@users.noreply.github.com>
2018-05-20 11:08:16 +0530

    Create hunter2.java

---
+++ hunter2.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication94;
+import java.util.*;
+/**
+ *
+ 
+ */
+public class JavaApplication94 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Scanner scn =new Scanner(System.in);
+        int n=scn.nextInt();
+        int[] a= new int[n];
+        
+        for(int i=0;i<n;i++)
+        {
+            a[i]=scn.nextInt();
+            
+        }
+        
+        
+        Arrays.sort(a);
+        for(int i=n-1;i>=0;i--)
+        {
+            System.out.print(a[i]);
+        }
+        
+    }
+    
+}
